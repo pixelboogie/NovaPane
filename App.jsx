da34f127f20df4f0853425c0a8b1110c99a44a56
pixelboogie <justamovie@yahoo.com>
2024-08-21 14:44:48 -0500

    Map items from data.jsx

--- App.jsx
+++ App.jsx
@@ -3,22 +3,31 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from "./data"
 
 
 function App() {
+
+  const cards = data.map(item => <Card 
+    key={item.id}
+    img={item.coverImg}
+    rating={item.stats.rating}
+    reviewCount={item.stats.reviewCount}
+    location={item.location}
+    title={item.title}
+    price={item.price}
+/>)
+
+
+
   return (
     <>
       <div>
         <Navbar />
-        <Card 
-          img="katie-zaferes.png"
-          rating="5.0"
-          reviewCount={6}
-          country="USA"
-          title="Life Lessons with Katie Zaferes"
-          price={136}
-        />
-       </div>
+        <section className="cards-list">
+                {cards}
+            </section>
+      </div>
     </>
   )
 }
