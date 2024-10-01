9ae06e3ddfb4114db361263132b45a2a3c30f2a6
pixelboogie <justamovie@yahoo.com>
2024-10-01 15:09:57 -0500

    Add Hero to app

--- App.jsx
+++ App.jsx
@@ -19,6 +19,7 @@ export default function App() {
   return (
       <div>
         <Navbar />
+        <Hero />
         <section className="cards-list">
                 {cards}
             </section>
